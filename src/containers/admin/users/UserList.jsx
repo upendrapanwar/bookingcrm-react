018feeb946e2bc2e8be5d349853344aebe1733ec
upendrapanwar <upendrapanwar456@gmail.com>
2024-10-16 19:08:13 +0530

    Uploading react file 16-10-2024

--- src/containers/admin/users/UserList.jsx
+++ src/containers/admin/users/UserList.jsx
@@ -14,12 +14,97 @@ const UserList = () => {
     const [allUsers, setAllUsers] = useState("");
     const [columns, setColumns] = useState([]);
     const [orderDataSet, setOrderDataSet] = useState([]);
-
+    const customStyles = {
+        pagination: {
+          style: {
+            display: 'flex',
+            justifyContent: 'center',
+          },
+          pageButtonsStyle: {
+            padding: '10px 15px',
+            margin: '0 5px',
+            backgroundColor: '#4caf50',
+            color: '#fff',
+            borderRadius: '5px',
+            border: 'none',
+            cursor: 'pointer',
+            transition: 'background-color 0.3s ease',
+          },
+          pageButtonHover: {
+            backgroundColor: '#45a049',
+          },
+          pageButtonDisabled: {
+            backgroundColor: '#ccc',
+            cursor: 'not-allowed',
+          },
+        },
+        
+      };
+    // Custom text for the pagination buttons
+    const paginationComponentOptions = {
+        rowsPerPageText: 'Rows per page:',
+        rangeSeparatorText: 'of',
+        noRowsPerPage: false,
+        selectAllRowsItem: true,
+        selectAllRowsItemText: 'All',
+        previousPageText: '<< Previous',  // Custom Previous button text
+        nextPageText: 'Next >>',          // Custom Next button text
+    };  
     useEffect(() => {
         //dispatch(getLeadsContent());
         getAllUsers();
     }, []);
-     /**
+
+    /***********************************************************************/
+    /***********************************************************************/
+    /**
+     * Handle datatable checkbox check
+     * 
+     */
+    const handleChange = (state) => {
+        // You can use setState or dispatch with something like Redux so we can use the retrieved data
+        console.log('Selected Rows: ', state.selectedRows);
+    };
+    /***********************************************************************/
+    /***********************************************************************/
+    /**
+     * Manage Status Update of user
+     * 
+     * @param id
+     * @return Object|null
+     * 
+    */
+    const handleStatusUpdate = (id) => {
+        console.log('Update id: ',id);
+    }
+    /***********************************************************************/
+    /***********************************************************************/
+    /**
+     * Manage Update of user by id
+     * 
+     * @param id
+     * @return Object|null
+     * 
+    */
+    const handleEdit = (id) => {
+        console.log('Update id: ',id);
+    }
+    /***********************************************************************/
+    /***********************************************************************/
+    /**
+     * Manage delete selected user by id
+     * 
+     * @param id
+     * @return Object|null
+     * 
+    */
+    const handleDeleteSeletedData = (id) => {
+        console.log('Update id: ',id);
+    }
+    /***********************************************************************/
+    /***********************************************************************/
+    
+    /**
      * Get Users courses list
      * 
      */
@@ -41,8 +126,8 @@ const UserList = () => {
                                 email: value.email,
                                 role: value.role,
                                 profile_image_url: (value.profile_image_url !=null) ? value.profile_image_url : <AvatarComponent />,
-                                isActive: (value.isActive === true) ? 'Active' : 'Inactive',
-                                createdAt: value.createdAt,
+                                isActive: (value.isActive === true) ? <div className="flex items-center"><div className="h-2.5 w-2.5 rounded-full bg-green-400 mr-2"></div><span>Active</span></div> : <div className="flex items-center"><div className="h-2.5 w-2.5 rounded-full bg-red-500 mr-2"></div><span>Inactive</span></div>,
+                                createdAt: value.createdAt
                             });
                           });
                         var columnsData = [
@@ -80,7 +165,7 @@ const UserList = () => {
                               {
                                 name: "Status",
                                 selector: (row, i) => row.isActive,
-                                cell: (row) => <span>{row.isActive}</span>,
+                                cell: (row) => row.isActive,
                                 sortable: true,
                               },
                             {
@@ -96,7 +181,27 @@ const UserList = () => {
                                 },
                                 sortable: true,
                               },
-                              
+                              {
+                                name: "Actions",
+                                cell: (row) => (
+                                  <>
+                                    <button
+                                      onClick={() => handleEdit(row.id)}
+                                      className="items-center px-3 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                                    >
+                                        <svg class="svgclass w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path><path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path></svg>
+                                        <span className="editBtn">Edit</span>
+                                    </button>
+                                    <button
+                                      className="items-center px-3 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900"
+                                      onClick={() => handleDeleteSeletedData(row._id)}
+                                    >
+                                        <svg class="svgclass w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path></svg>
+                                      <span className="deleteBtn">Delete</span>
+                                    </button>
+                                  </>
+                                ),
+                              },
                            
                           ];
                         setColumns(columnsData);
@@ -152,7 +257,7 @@ const UserList = () => {
                                     <h1 className="text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">All users</h1>
                                 </div>
                                 <div className="sm:flex">
-                                    <div className="items-center hidden mb-3 sm:flex sm:divide-x sm:divide-gray-100 sm:mb-0 dark:divide-gray-700">
+                                    {/*<div className="items-center hidden mb-3 sm:flex sm:divide-x sm:divide-gray-100 sm:mb-0 dark:divide-gray-700">
                                         <form className="lg:pr-3" action="#" method="GET">
                                             <label for="users-search" className="sr-only">Search</label>
                                             <div className="relative mt-1 lg:w-64 xl:w-96">
@@ -173,49 +278,49 @@ const UserList = () => {
                                                 <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z"></path></svg>
                                             </Link>
                                         </div>
-                                    </div>
+                                    </div>*/}
                                     <div className="flex items-center ml-auto space-x-2 sm:space-x-3">
                                         <button type="button" data-modal-target="add-user-modal" data-modal-toggle="add-user-modal" className="inline-flex items-center justify-center w-1/2 px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 sm:w-auto dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                                             <svg className="w-5 h-5 mr-2 -ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
                                             Add user
                                         </button>
-                                        <Link to={"#"} className="inline-flex items-center justify-center w-1/2 px-3 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-primary-300 sm:w-auto dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">
+                                        {/*<Link to={"#"} className="inline-flex items-center justify-center w-1/2 px-3 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-primary-300 sm:w-auto dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">
                                             <svg className="w-5 h-5 mr-2 -ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z" clip-rule="evenodd"></path></svg>
                                             Export
-                                        </Link>
+                                        </Link>*/}
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="overflow-x-auto w-full">
-                            <DataTableExtensions
-                                columns={columns}
-                                data={orderDataSet}
-                            >
-                                <DataTable
-                                title="Table"
-                                noHeader
-                                defaultSortField="id"
-                                defaultSortAsc={false}
-                                pagination
-                                highlightOnHover
-                                customStyles={{
-                                    rows: {
-                                    style: {
-                                        minHeight: '50px', // override the row height
-                                    }
-                                    },
-                                    cells: {
-                                    style: {
-                                        whiteSpace: 'nowrap', // add white-space nowrap to prevent wrapping
-                                    },
-                                    },
-                                }}
-                                />
-                            </DataTableExtensions>
+                        <div className="flex flex-col">
+                            <div className="overflow-x-auto w-full">
+                                <div className="min-w-full align-middle">
+                                    <div class="overflow-hidden shadow">
+                                        <DataTableExtensions
+                                        columns={columns}
+                                        data={orderDataSet}
+                                    >
+                                        <DataTable
+                                        title="Table"
+                                        selectableRows
+                                        noHeader
+                                        defaultSortField="id"
+                                        defaultSortAsc={false}
+                                        pagination
+                                        paginationComponentOptions={paginationComponentOptions}
+                                        highlightOnHover
+                                        Clicked
+                                        Selected={handleChange}
+                                        customStyles={customStyles}
+                                        />
+                                    </DataTableExtensions>             
+                                    </div>
+                                </div>                
 
+                            </div>
                         </div>
-                        <div className="sticky bottom-0 right-0 items-center w-full p-4 bg-white border-t border-gray-200 sm:flex sm:justify-between dark:bg-gray-800 dark:border-gray-700">
+                        
+                        {/*<div className="sticky bottom-0 right-0 items-center w-full p-4 bg-white border-t border-gray-200 sm:flex sm:justify-between dark:bg-gray-800 dark:border-gray-700">
                             <div className="flex items-center mb-4 sm:mb-0">
                                 <Link to={"#"} className="inline-flex justify-center p-1 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
                                     <svg className="w-7 h-7" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
@@ -235,7 +340,7 @@ const UserList = () => {
                                     <svg className="w-5 h-5 ml-1 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
                                 </Link>
                             </div>
-                        </div>
+                        </div>*/}
 
 
                         <div className="fixed left-0 right-0 z-50 items-center justify-center hidden overflow-x-hidden overflow-y-auto top-4 md:inset-0 h-modal sm:h-full" id="edit-user-modal">
